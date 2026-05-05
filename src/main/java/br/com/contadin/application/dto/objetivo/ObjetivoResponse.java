9f9794ca069cba86a8667069520f25a8c0202c0f
joaoGlima09 <joao.glima@sptech.school>
2026-05-05 09:13:46 -0300

    refactor: atualizando models dos objetivos

--- src/main/java/br/com/contadin/application/dto/objetivo/ObjetivoResponse.java
+++ src/main/java/br/com/contadin/application/dto/objetivo/ObjetivoResponse.java
@@ -1,17 +1,28 @@
 package br.com.contadin.application.dto.objetivo;
 
+import br.com.contadin.domain.enums.PrioridadeObjetivo;
+import br.com.contadin.domain.enums.StatusObjetivo;
+import br.com.contadin.domain.enums.TipoObjetivo;
+
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 public record ObjetivoResponse(
         UUID id,
         String nome,
+        String descricao,
+        TipoObjetivo tipoObjetivo,
         BigDecimal valor,
-        Date dataFimObjetivo,
+        BigDecimal realizado,
+        BigDecimal percentual,
+        StatusObjetivo status,
+        String mensagemStatus,
+        LocalDate dataInicio,
+        LocalDate dataFim,
+        PrioridadeObjetivo prioridade,
+        UUID fkCategoria,
         LocalDateTime criadoEm,
-        LocalDateTime atualizadoEm,
-        UUID fkCategoria
-) {
-}
+        LocalDateTime atualizadoEm
+) {}
