600b0c7f00552e965e65724235040ca4cd03233b
Kauan Paschoal <kauanpaschoal.santos@gmail.com>
2026-05-15 23:07:50 -0300

    feat: adicionando model de saldo diario e alteração em instituição para inicializar saldo

--- src/main/java/br/com/contadin/infrastructure/persistence/entity/InstituicaoEntity.java
+++ src/main/java/br/com/contadin/infrastructure/persistence/entity/InstituicaoEntity.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -32,6 +33,9 @@ public class InstituicaoEntity {
 
     @Column(name = "fk_usuario", nullable = false)
     private  UUID fkUsuario;
+
+    @Column(name = "saldo_inicial", precision = 19, scale = 4)
+    private BigDecimal saldoInicial;
 
     private  Boolean ativo;
 
